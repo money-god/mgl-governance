2e15b5b793a09cb9400bf6bb0e38b255bce66124
oddaf <oddaf@proton.me>
2023-06-14 17:28:21 -0300

    solidity .8 dsPause + tests

--- test/Governor.t.sol
+++ test/Governor.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.19;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../src/Governor.sol";
 
 contract CounterTest is Test {
     Counter public counter;
